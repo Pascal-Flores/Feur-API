57f77a1ca19a6fd22e722b8e9ff63f9bfb335df3
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2023-04-08 22:56:58 +0200

    full manga request handler done

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as oak from "https://deno.land/x/oak/mod.ts";
 import { Body } from "https://deno.land/x/oak@v12.1.0/body.ts";
 import FullMangaRequestSanitizer from "./modules/Sanitizers/FullMangaRequestSanitizer.ts";
-import InvalidRequestError from "./modules/Errors/InvalidRequestError.ts";
+import Downloader from "./modules/Downloaders/Downloader.ts";
+import DownloaderFactory from "./modules/Downloaders/DownloaderFactory.ts";
+import FullMangaRequest from "./modules/SanitizedUserInputs/FullMangaRequest.ts";
 
 const router = new oak.Router();
 
@@ -14,9 +16,9 @@ router.post("/download/full-manga/", async (ctx) => {
         ctx.response.body = "Invalid request";
         return;
     }
-
+    let sanitizedRequest : FullMangaRequest;
     try {
-        const sanitizedRequest = FullMangaRequestSanitizer.sanitize(values);
+        sanitizedRequest = FullMangaRequestSanitizer.sanitize(values);
     }
     catch (error) {
         ctx.response.status = 400;
@@ -24,7 +26,17 @@ router.post("/download/full-manga/", async (ctx) => {
         return;
     }
 
+    try {
+        const downloader : Downloader = await DownloaderFactory.createDownloader(sanitizedRequest.website);
+        await downloader.downloadFullManga(sanitizedRequest);
+    }
+    catch (error) {
+        ctx.response.status = 500;
+        ctx.response.body = error.message;
+        return;
+    }
     
+
 
 });
 
